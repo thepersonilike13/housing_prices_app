3398cacb2a744f4aca2a60893628d6a182d5ea3c
matheuscamposm <matheuscamposmattos@id.uff.br>
2023-04-14 18:35:54 -0300

    .

--- app/utils/combiner.py
+++ app/utils/combiner.py
@@ -4,6 +4,10 @@ import pandas as pd
 
 rooms_ix, bedrooms_ix, population_ix, households_ix = 3, 4, 5, 6
 class CombinedAttributesAdder(BaseEstimator, TransformerMixin):
+
+    DISTANCE_CRS = 3857
+    CRS = 4326
+
     def __init__(self, add_bedrooms_per_room = True): # no *args or **kargs 
         self.add_bedrooms_per_room = add_bedrooms_per_room
 
@@ -11,37 +15,42 @@ class CombinedAttributesAdder(BaseEstimator, TransformerMixin):
         california_cities = pd.read_csv('app/utils/cal_cities_lat_long.csv')
         geometry_cal = gpd.points_from_xy(california_cities['Longitude'], california_cities['Latitude'])
         self.points_cal = gpd.GeoDataFrame(california_cities, geometry=geometry_cal)
+        self.points_cal.set_crs(self.CRS, inplace=True)
+
 
         # coastline
         self.coastline = gpd.read_file('data/coastline/US_Westcoast.shp')[['geometry']]
-        target_crs = 'EPSG:4326'
-        self.coastline = self.coastline.to_crs(target_crs)
-
+        self.coastline = self.coastline.to_crs(crs=self.CRS)
 
         self.points = None
     
-    def add_nearest_cities(self, data):
-        joined_data = gpd.sjoin_nearest(self.points, self.points_cal, how='left', 
+    def add_nearest_cities(self):
+        nearest_cities = gpd.sjoin_nearest(self.points, self.points_cal, how='left', 
                                         distance_col='distance_nearest_city')
-        joined_data = joined_data.rename(columns={'Name': 'nearest_city'})
+        nearest_cities.drop(columns=['index_right', 'geometry'], inplace=True)
+        nearest_cities = nearest_cities.rename(columns={'Name': 'nearest_city'})
 
-        return joined_data
-    def add_ocean_distance(self, data):
-        data['distance_to_ocean'] = self.points.distance(self.coastline.geometry.unary_union)
-        
-        return data
+        return nearest_cities
+    
+    def add_ocean_distance(self):
+        nearest_coastline = gpd.sjoin_nearest(self.points.to_crs(crs=self.DISTANCE_CRS), self.coastline.to_crs(crs=self.DISTANCE_CRS), distance_col='distance_to_ocean')
+        nearest_coastline.to_crs(self.CRS, inplace=True)
+        nearest_coastline.drop(columns=['index_right', 'geometry'], inplace=True)
 
+        return nearest_coastline
 
     def fit(self, X, y=None):
         geometry = gpd.points_from_xy(X['lon'], X['lat'])
         self.points = gpd.GeoDataFrame(X, geometry=geometry)
+        self.points.set_crs(self.CRS, inplace=True)
 
         return self
     def transform(self, X, y=None):
-        joined_data = self.add_nearest_cities(X)
-        #joined_data = self.add_ocean_distance(joined_data)
+        joined_data = self.add_ocean_distance()
+        joined_data = self.add_nearest_cities()
 
-        X = joined_data.drop(columns=['lon','lat','geometry', 'index_right','Latitude','Longitude'])
+
+        X = joined_data.drop(columns=['geometry','lon','lat','Latitude','Longitude'])
 
         # removing generated duplicated locations (when there are more than one nearest city)
         X = X[~X.index.duplicated(keep='first')]  
