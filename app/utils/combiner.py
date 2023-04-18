bc6a699c8e81c10296b99e4e117332d299b003ff
matheuscamposm <matheuscamposmattos@id.uff.br>
2023-04-17 22:53:35 -0300

    .

--- app/utils/combiner.py
+++ app/utils/combiner.py
@@ -5,7 +5,7 @@ import pandas as pd
 rooms_ix, bedrooms_ix, population_ix, households_ix = 3, 4, 5, 6
 class CombinedAttributesAdder(BaseEstimator, TransformerMixin):
 
-    DISTANCE_CRS = 3857
+    DISTANCE_CRS = 32643
     CRS = 4326
 
     def __init__(self, add_bedrooms_per_room = True): # no *args or **kargs 
@@ -22,42 +22,58 @@ class CombinedAttributesAdder(BaseEstimator, TransformerMixin):
         self.coastline = gpd.read_file('data/coastline/US_Westcoast.shp')[['geometry']]
         self.coastline = self.coastline.to_crs(crs=self.CRS)
 
-        self.points = None
+    @staticmethod
+    def dataframe_to_geo(data: pd.DataFrame):
+        geometry = gpd.points_from_xy(data['lon'], data['lat'])
+        data = gpd.GeoDataFrame(data).set_geometry(geometry).set_crs(CombinedAttributesAdder.CRS)
+
+        return data
     
-    def add_nearest_cities(self):
-        nearest_cities = gpd.sjoin_nearest(self.points, self.points_cal, how='left', 
-                                        distance_col='distance_nearest_city')
-        nearest_cities.drop(columns=['index_right', 'geometry'], inplace=True)
+    def add_nearest_cities(self, data):
+
+        if not isinstance(data, gpd.GeoDataFrame):
+            data = self.dataframe_to_geo(data)
+    
+        nearest_cities = gpd.sjoin_nearest(
+            data.to_crs(crs=self.DISTANCE_CRS), 
+            self.points_cal.to_crs(crs=self.DISTANCE_CRS), 
+            how='left', distance_col='distance_nearest_city'
+        ).to_crs(self.CRS).drop(columns=['index_right'])
         nearest_cities = nearest_cities.rename(columns={'Name': 'nearest_city'})
+
+        nearest_cities = nearest_cities[~nearest_cities.index.duplicated(keep='first')]
+
 
         return nearest_cities
     
-    def add_ocean_distance(self):
-        nearest_coastline = gpd.sjoin_nearest(self.points.to_crs(crs=self.DISTANCE_CRS), self.coastline.to_crs(crs=self.DISTANCE_CRS), distance_col='distance_to_ocean')
-        nearest_coastline.to_crs(self.CRS, inplace=True)
-        nearest_coastline.drop(columns=['index_right', 'geometry'], inplace=True)
+    def add_ocean_distance(self, data):
+
+        if not isinstance(data, gpd.GeoDataFrame):
+            data = self.dataframe_to_geo(data)
+
+        nearest_coastline = gpd.sjoin_nearest(
+            data.to_crs(crs=self.DISTANCE_CRS), 
+            self.coastline.to_crs(crs=self.DISTANCE_CRS), 
+            distance_col='distance_to_ocean'
+        ).to_crs(self.CRS).drop(columns='index_right')
+
+        nearest_coastline = nearest_coastline[~nearest_coastline.index.duplicated(keep='first')]
 
         return nearest_coastline
 
     def fit(self, X, y=None):
-        geometry = gpd.points_from_xy(X['lon'], X['lat'])
-        self.points = gpd.GeoDataFrame(X, geometry=geometry)
-        self.points.set_crs(self.CRS, inplace=True)
-
         return self
-    def transform(self, X, y=None):
-        joined_data = self.add_ocean_distance()
-        joined_data = self.add_nearest_cities()
 
+    def transform(self, X, y=None):
+        data = self.dataframe_to_geo(X)
+
+        joined_data = self.add_nearest_cities(data)
+        #joined_data = self.add_ocean_distance(joined_data)
 
         X = joined_data.drop(columns=['geometry','lon','lat','Latitude','Longitude'])
+        X = X.assign(
+            rooms_per_household=X.total_rooms / X.households,
+            bedrooms_per_room=X.total_bedrooms / X.total_rooms if self.add_bedrooms_per_room else None
+        )
 
-        # removing generated duplicated locations (when there are more than one nearest city)
-        X = X[~X.index.duplicated(keep='first')]  
-
-        X['rooms_per_household'] = X.total_rooms / X.households
-        #X['population_per_household'] = X.population / X.households
-        if self.add_bedrooms_per_room:
-            X['bedrooms_per_room'] = X.total_bedrooms / X.total_rooms
-            
         return X
